37abc31fab7f2b59bee118fcdfd927b41295d892
danushsingla <139186991+danushsingla@users.noreply.github.com>
2024-06-22 19:59:56 -0700

    Update create-user.js

--- pages/api/create-user.js
+++ pages/api/create-user.js
@@ -1,6 +1,10 @@
 import { supabase } from '../../lib/supabaseClient';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins, adjust as needed
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
   if (req.method === 'POST') {
     const name = req.body.name;
     const { data, error } = await supabase
